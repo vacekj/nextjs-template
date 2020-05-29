df5e51d27474f294d21bd7d186d1313ae59e8365
Josef Vacek <vacekj@outlook.com>
2020-05-29 14:12:55 +0200

    fix: missing component and misplaced baseUrl

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
 import Layout from "components/Layout";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TailwindCSS + TypeScript + Prettier">
-    <h1>Hello Next.js 👋</h1>
-  </Layout>
+	<Layout title="Home | Next.js + TailwindCSS + TypeScript + Prettier">
+		<h1>Hello Next.js 👋</h1>
+	</Layout>
 );
 
 export default IndexPage;
